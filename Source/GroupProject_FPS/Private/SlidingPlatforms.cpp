8a932ce35ea1396fc0c1b88b7050622b1795230e
Jedgar402 <jackcedgar20@gmail.com>
2023-12-03 22:20:19 +0000

    Sliding Paltform

---
+++ Source/GroupProject_FPS/Private/SlidingPlatforms.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SlidingPlatforms.h"
+
+// Sets default values
+ASlidingPlatforms::ASlidingPlatforms()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASlidingPlatforms::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASlidingPlatforms::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
